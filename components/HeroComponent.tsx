d140a1b27e9b2bbfcccd10d6e3b71de7e2213705
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-17 19:04:45 +0200

    style: made hero images bigger

--- components/HeroComponent.tsx
+++ components/HeroComponent.tsx
@@ -16,12 +16,12 @@ const HeroComponent = ({
 }: Props) => {
     return (
         <div
-            className={`${dark ? "bg-slate-800 text-white" : ""} ${
+            className={`${dark ? "bg-slate-900 text-white" : ""} ${
                 reverse && "order-last"
-            } grid min-h-[50vh] gap-12 py-12 px-4 md:grid-cols-8 md:p-12 xl:px-32`}
+            } grid min-h-[50vh] gap-12 p-12 px-4 md:grid-cols-8 md:p-12 lg:min-h-[80vh] xl:px-32`}
         >
             <div className="flex items-center justify-center md:col-span-3  lg:justify-start xl:col-span-4">
-                <div className=" pb-12 text-base md:pb-0 lg:text-xl ">
+                <div className="  text-base md:pb-0 lg:text-xl ">
                     {children}
                 </div>
             </div>
@@ -30,7 +30,7 @@ const HeroComponent = ({
                     reverse ? "md:order-first " : " md:justify-center"
                 } flex items-center justify-start md:col-span-5   xl:col-span-4`}
             >
-                <div className="xl:w-2/3">
+                <div className="xl:h-2/3">
                     <Image src={img} alt="Search Food" />
                 </div>
             </div>
