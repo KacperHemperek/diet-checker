d98ca4fcc4f670d75dac26abe5c87adb9146fbf6
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-18 13:08:28 +0200

    fix: removed unwanted files

--- pages/api/get_token.ts
+++ pages/api/get_token.ts
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
-import { url } from "inspector";
 
 type Data = {
     name: string;
