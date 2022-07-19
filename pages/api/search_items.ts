ae2411eff6e1b773b4306c66477c0bf4a334a819
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-19 13:50:26 +0200

    chore: removed api keys

--- pages/api/search_items.ts
+++ pages/api/search_items.ts
@@ -7,23 +7,27 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    try {
-        const response = await fetch(
-            "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=pasta",
-            {
-                headers: {
-                    "X-RapidAPI-Key": `${process.env.NEXT_APP_API_KEY}`,
-                    "X-RapidAPI-Host": `${process.env.NEXT_APP_API_HOST}`,
-                },
-            }
-        );
+    if (req.method === "POST") {
+        try {
+            const response = await fetch(
+                "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=pasta",
+                {
+                    headers: {
+                        "X-RapidAPI-Key": `${process.env.NEXT_APP_API_KEY}`,
+                        "X-RapidAPI-Host": `${process.env.NEXT_APP_API_HOST}`,
+                    },
+                }
+            );
 
-        const data = await response.json();
-        res.status(200).json({ ...data });
-    } catch (error: any) {
-        console.error(error);
-        if (error?.message) {
-            res.status(500).json({ error: error.message });
+            const data = await response.json();
+            res.status(200).json({ ...data });
+        } catch (error: any) {
+            console.error(error);
+            if (error?.message) {
+                res.status(500).json({ error: error.message });
+            }
         }
+    } else {
+        res.status(401).json({ message: "Wrong method" });
     }
 }
