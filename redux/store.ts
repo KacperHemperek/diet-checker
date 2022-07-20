a5790b9255c161060483c6de0d851d031b6bb567
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-20 10:24:06 +0200

    feat: redux store is saved in local storage and rehydrates after reload of page

--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 import { createLogger } from "redux-logger";
+import { persistCombineReducers, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const logger = createLogger();
 
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistCombineReducers(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: [logger],
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
