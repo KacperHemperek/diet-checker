484be8df237092602ecfbd61f4f73fc116e4a041
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-21 21:36:32 +0200

    feat: sign up form on mobile has description

--- pages/register.tsx
+++ pages/register.tsx
@@ -59,32 +59,31 @@ const login = () => {
             <h1 className=" mb-4 text-xl font-semibold">
                 <span className="text-green-500">Create</span> Account
             </h1>
-            <div>
-                <p className="mb-4">
-                    Save your diet plans to access them whenever you need them.{" "}
-                </p>
-                <h2 className="mb-2 text-lg">
-                    <span className="font-semibold text-green-500">Main</span>{" "}
-                    features:{" "}
-                </h2>
-                <ul className="mb-8 ml-2">
-                    {benefits.map((item) => (
-                        <li className="mb-1 flex items-baseline gap-2 fill-green-500">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 448 512"
-                                className="h-4 w-4 translate-y-[2px]"
-                            >
-                                <path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" />
-                            </svg>
-                            {item}
-                        </li>
-                    ))}
-                </ul>
-                <div className="flex w-full ">
-                    <div className="w-full xl:w-3/4">
-                        <Image src={descImg} />
-                    </div>
+
+            <p className="mb-4">
+                Save your diet plans to access them whenever you need them.{" "}
+            </p>
+            <h2 className="mb-2 text-lg">
+                <span className="font-semibold text-green-500">Main</span>{" "}
+                features:{" "}
+            </h2>
+            <ul className="mb-8 ml-2">
+                {benefits.map((item) => (
+                    <li className="mb-1 flex items-baseline gap-2 fill-green-500">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                            className="h-4 w-4 translate-y-[2px]"
+                        >
+                            <path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" />
+                        </svg>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+            <div className="flex w-full ">
+                <div className="w-full xl:w-3/4">
+                    <Image src={descImg} />
                 </div>
             </div>
         </div>
@@ -103,6 +102,35 @@ const login = () => {
                         </span>{" "}
                         Up
                     </h1>
+                    <div className="block lg:hidden">
+                        <h1 className=" mb-4 text-xl ">
+                            <span className="text-green-500">Create</span>{" "}
+                            Account
+                        </h1>
+
+                        <p className="mb-4">
+                            Save your diet plans to access them whenever you
+                            need them.{" "}
+                        </p>
+                        <h2 className="mb-2 text-lg">
+                            <span className=" text-green-500">Main</span>{" "}
+                            features:{" "}
+                        </h2>
+                        <ul className="mb-8 ml-2">
+                            {benefits.map((item) => (
+                                <li className="mb-1 flex items-baseline gap-2 fill-green-500">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 448 512"
+                                        className="h-4 w-4 translate-y-[2px]"
+                                    >
+                                        <path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" />
+                                    </svg>
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
                     <FormInput
                         label="Email"
