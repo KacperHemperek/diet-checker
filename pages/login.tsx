0f7e6e10973d205a6d4c83dddfcebfa623f3419b
michal-pawlicki <michalpawlicki02@gmail.com>
2022-07-21 11:25:41 +0200

    feat: signup form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import Layout from '../layouts/Layout'
+import React from "react";
+import Layout from "../layouts/Layout";
+import { useFormik } from "formik";
 
 const login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <Layout>
-        
-    </Layout>
-  )
-}
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
-export default login
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+
+        <label htmlFor="repeatPassword">Repeat Password</label>
+        <input
+          id="repeatPassword"
+          name="repeatPassword"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.repeatPassword}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </Layout>
+  );
+};
+
+export default login;
