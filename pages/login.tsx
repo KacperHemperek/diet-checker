60a5f3d099827e53faacb6f4f2ddabf436084031
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-21 12:10:59 +0200

    chore: add yup package

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,52 +1,57 @@
 import React from "react";
 import Layout from "../layouts/Layout";
 import { useFormik } from "formik";
-
+import * as Yup from "yup";
 const login = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      repeatPassword: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  return (
-    <Layout>
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
 
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          name="password"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
 
-        <label htmlFor="repeatPassword">Repeat Password</label>
-        <input
-          id="repeatPassword"
-          name="repeatPassword"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.repeatPassword}
-        />
 
-        <button type="submit">Submit</button>
-      </form>
-    </Layout>
-  );
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            repeatPassword: "",
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    console.log({ ...formik });
+    return (
+        <Layout>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="email">Email Address</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+
+                <label htmlFor="repeatPassword">Repeat Password</label>
+                <input
+                    id="repeatPassword"
+                    name="repeatPassword"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.repeatPassword}
+                />
+
+                <button type="submit">Submit</button>
+            </form>
+        </Layout>
+    );
 };
 
 export default login;
