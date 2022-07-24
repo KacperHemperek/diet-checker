144baa63ddc9b6600ec404fb0bf3985e40079b4e
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-24 23:31:31 +0200

    fix: removed package that changed default colors on app

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Layout from "../layouts/Layout";
-import AboutAccordion from "../components/AboutAccordion";
-import Image from "next/image";
-import Link from "next/link";
-import HeroImage from "../public/undraw_diet_ghvw.svg";
+
 
 const about = () => {
   return (
     <Layout>
       <div className="p-10">
-        <AboutAccordion />
+
       </div>
     </Layout>
   );
