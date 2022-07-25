fc1efa619e0b275e40c773499f0239eed91bcc7a
michal-pawlicki <michalpawlicki02@gmail.com>
2022-07-25 13:12:21 +0200

    feat: login functionality

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,15 +6,37 @@ import CustomFormWrapper from "../components/CustomFormWrapper";
 import Link from "next/link";
 import descImg from "../public/login_form_img.svg";
 import Image from "next/image";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utils/firebase.utils";
+import { useRouter } from "next/router";
+import { setRegistrationError, setUser } from "../redux/features/userData";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const userError = useSelector((state: RootState) => state.user.registerError);
+  const router = useRouter();
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: async (values) => {
+      try {
+        const userCred = await signInWithEmailAndPassword(
+          auth,
+          values.email,
+          values.password
+        );
+        dispatch(setUser(userCred.user));
+        dispatch(setRegistrationError(""));
+        await router.push("/");
+      } catch (e: any) {
+        console.log(e.code);
+        values.password = "";
+        dispatch(setRegistrationError(e.code));
+      }
     },
   });
 
@@ -26,7 +48,7 @@ const Login = () => {
       <p className="mb-4">Log in to access your personalised diet plans. </p>
       <div className="flex w-full ">
         <div className="w-full">
-          <Image src={descImg} alt="register page image"/>
+          <Image src={descImg} alt="register page image" />
         </div>
       </div>
     </div>
@@ -54,12 +76,19 @@ const Login = () => {
             value={formik.values.password}
           />
 
-          <button
-            type="submit"
-            className="mb-6 rounded-full bg-green-500 px-4 py-2 text-white"
-          >
-            Log In
-          </button>
+          {userError && (
+            <p className="mb-6 w-full rounded-lg border border-red-500 bg-red-200 p-2 text-center text-red-500 first-letter:uppercase">
+              {userError}
+            </p>
+          )}
+          <div className="flex w-full justify-center">
+            <button
+              type="submit"
+              className=" mb-6 rounded-full bg-green-500 px-4 py-2 text-white"
+            >
+              Log In
+            </button>
+          </div>
           <p className="text-sm">
             If you don&apos;t have an account you can{" "}
             <Link href="/register">
