3abd787a38eb98fc8e0cdc0f0e5843cbbbe7ab84
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-25 10:28:15 +0200

    fix: add userReducer to rootReducer

--- redux/rootReducer.ts
+++ redux/rootReducer.ts
@@ -1,6 +1,8 @@
 import searchReducer from "./features/searchData";
+import userReducer from "./features/userData"
 const rootReducer = {
     search: searchReducer,
+    user: userReducer
 };
 
 export default rootReducer;
