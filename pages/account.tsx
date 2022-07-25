31fd92039926a99e8d650e594616daf0256a5ae1
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-25 17:25:10 +0200

    feat: add account page

---
+++ pages/account.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NextPage} from "next";
+import Layout from "../layouts/Layout";
+
+
+const Account: NextPage = () =>  {
+    return (
+      <Layout>
+        <div>
+          account
+        </div>
+      </Layout>
+    );
+}
+
+export default Account;
