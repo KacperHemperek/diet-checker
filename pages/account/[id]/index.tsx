730f89f51a33b1ec82c94727c2f35abe5540b5ad
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-26 15:38:07 +0200

    feat: account page displays user information from firestore

--- pages/account/[id]/index.tsx
+++ pages/account/[id]/index.tsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
 import Layout from "../../../layouts/Layout";
 import ProfileData from "../../../components/ProfileData";
 import FoodCard from "../../../components/FoodCard";
 
-const Account: NextPage = () => {
+type Recipe = {
+  name: string;
+  calorie: number;
+};
+
+type Props = {
+  email: string;
+  age?: number;
+  recipes?: Recipe[];
+  height?: number;
+  weight?: number;
+  name?: string;
+};
+
+const Account: NextPage<Props> = (props) => {
   return (
     <Layout>
       <div className="mx-4 grid gap-12 pt-8 md:mx-12 md:grid-cols-12  xl:mx-32">
-        <ProfileData />
+        <ProfileData
+          email={props.email}
+          name={props.name}
+          age={props.age}
+          height={props.height}
+          weight={props.weight}
+        />
         <div className=" md:col-span-6 lg:col-span-7 xl:col-span-8">
           <h2 className="mb-6 text-2xl font-semibold">
             <span className="text-green-500">Your</span> favorite recipes
@@ -46,5 +66,17 @@ const Account: NextPage = () => {
 };
 
 export default Account;
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const { id } = context.query;
+  const response = await fetch(
+    `${process.env.APP_URL}/api/get_user_by_id?uid=${id}`
+  );
+  const data = await response.json();
+
+  return {
+    props: { ...data },
+  };
+};
 
 //get user info from  (create endpoint that will get info from firebase) and use getStaticProps and getStaticPaths to fetch data
