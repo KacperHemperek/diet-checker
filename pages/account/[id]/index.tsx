e665c3f19742513a327791c0e79e8c7ea2f98721
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-26 12:08:58 +0200

    feat: created nested route for user settings

--- pages/account/[id]/index.tsx
+++ pages/account/[id]/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NextPage } from "next";
-import Layout from "../../layouts/Layout";
+import Layout from "../../../layouts/Layout";
 
 const Account: NextPage = () => {
   return (
---
+++ pages/account/[id]/settings.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../../layouts/Layout";
+
+function Settings() {
+  return <Layout>Settings</Layout>;
+}
+
+export default Settings;
