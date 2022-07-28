46b30491aa755d3a5537fb02084b8ae9fa8ca34d
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-28 19:31:59 +0200

    style: made modal card as a custom component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,7 +3,7 @@ import React, { Dispatch, ReactNode, SetStateAction, useEffect } from "react";
 type Props = {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
-  children: ReactNode | ReactNode[];
+  children: ReactNode;
 };
 
 const Modal = ({ isOpen, setIsOpen, children }: Props) => {
@@ -21,7 +21,7 @@ const Modal = ({ isOpen, setIsOpen, children }: Props) => {
         className="fixed top-0 left-0 z-0 h-screen w-screen bg-black/30 backdrop-blur-[2px] "
         onClick={() => setIsOpen(false)}
       ></div>
-      <div className="fixed left-1/2 top-1/2 z-10 flex min-w-[90vw] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg bg-white p-4 shadow-lg ">
+      <div className="fixed left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 ">
         {children}
       </div>
     </div>
