c8eb9976fcfd09da7feb309732740bb33f19c2ba
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-08-03 13:57:18 +0200

    feat: endpoint for adding recipe to favorite updates list of favorite recipes
    todo: cover all cases

--- pages/api/add_favorite.tsx
+++ pages/api/add_favorite.tsx
@@ -1,4 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { doc, getDoc, updateDoc } from "@firebase/firestore";
+import { db } from "../../utils/firebase.utils";
+import { Recipe } from "../../interface/Recipe";
+import { arrayUnion } from "@firebase/firestore";
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,6 +16,56 @@ export default async function handler(
   }
   if (!itemId) {
     res.status(400).json({ message: "product id is required" });
+    return;
+  }
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/${itemId}/information?apiKey=${process.env.SPOONACULAR_API_KEY}&includeNutrition=true`
+    );
+    const data = await response.json();
+
+    try {
+      const docRef = await doc(db, "users", String(uid));
+      const userSnap = await getDoc(docRef);
+      const userData = userSnap.data();
+
+      if (userData?.recipes) {
+        const ids = userData.recipes.map((item: Recipe) => item.id);
+        console.log(ids);
+        if (ids.includes(Number(itemId))) {
+          res.status(200).json({ message: "recipe is already in favorite" });
+          return;
+        }
+        const newRecipe: Recipe = {
+          id: data.id,
+          name: data.title,
+          cal: data.nutrition.nutrients[0].amount,
+          img: data.image,
+          vegan: data.vegan,
+          vegetarian: data.vegetarian,
+          diets: data.diets,
+        };
+
+        await updateDoc(docRef, {
+          recipes: arrayUnion(newRecipe),
+        });
+
+        res.status(200).json({ message: "successfully added recipe" });
+        return;
+      }
+    } catch (e) {
+      console.error(e);
+      res
+        .status(500)
+        .json({ message: "couldn't add that recipe to favorites" });
+    }
+
+    return;
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "internal server error, couldn't fetch data from spoonacular",
+    });
     return;
   }
 }
