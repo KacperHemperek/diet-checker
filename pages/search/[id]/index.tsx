88b03e8618719e64391c8615c877de48db23d297
michal-pawlicki <michalpawlicki02@gmail.com>
2022-08-05 18:46:32 +0200

    feat: ingredient list on recipe page

--- pages/search/[id]/index.tsx
+++ pages/search/[id]/index.tsx
@@ -30,10 +30,30 @@ const RecipePage: NextPage = () => {
           <div className="flex-grow border border-red-500">{/* charts */}</div>
         </div>
 
-        <div className="md:col-span-6 md:p-10 lg:col-span-7 xl:col-span-8 ">
-          <h2 className="mb-6 text-2xl font-semibold">
-            <span className="text-green-500">Recipe</span> summary
-          </h2>
+        <div className="grid grid-cols-12 gap-12 ">
+          <div className="col-span-5 rounded-lg border p-4">
+            <h2 className="mb-6 text-2xl font-semibold">Ingredients</h2>
+            {data.extendedIngredients.map((item, index) => (
+              <li
+                className="mb-1 flex items-baseline gap-2 fill-green-500"
+                key={index}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                  className="h-4 w-4 translate-y-[2px]"
+                >
+                  <path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" />
+                </svg>
+                {item.original}
+              </li>
+            ))}
+          </div>
+          <div className="col-span-7 rounded-lg border p-4">
+            <h2 className="mb-6 text-2xl font-semibold">
+              <span className="text-green-500">Recipe</span> summary
+            </h2>
+          </div>
         </div>
       </div>
     </Layout>
