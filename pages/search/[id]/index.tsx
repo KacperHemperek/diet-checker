e253ff260aa7c22dea6b8ad7439d9dd2967a2874
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-08-05 18:42:48 +0200

    style: charts alignment changed

--- pages/search/[id]/index.tsx
+++ pages/search/[id]/index.tsx
@@ -9,15 +9,17 @@ const RecipePage: NextPage = () => {
   return (
     <Layout>
       <div className="mx-4  md:mx-12 xl:mx-32">
-        <div className="flex flex-col py-8">
-          <div className="">
+        <div className="flex flex-col py-8 lg:flex-row">
+          <div className="flex flex-col lg:flex-row">
             <div className="max-w-1/2 flex flex-col items-center">
               <img
-                className="mb-2 aspect-square w-28 rounded-full border-2 border-green-500 "
+                className="mb-2 aspect-square w-28 rounded-full border-2 border-green-500 lg:mb-4 "
                 src={data.image}
                 alt={data.title}
               />
-              <h1 className="text-center font-semibold">{data.title}</h1>
+              <h1 className="text-center font-semibold lg:text-xl">
+                {data.title}
+              </h1>
             </div>
             <RecipeTags
               isCheap={data.cheap}
@@ -27,10 +29,15 @@ const RecipePage: NextPage = () => {
               isVegetarian={data.vegetarian}
             />
           </div>
-          <div className="flex-grow border border-red-500">{/* charts */}</div>
+          <div className="flex flex-grow flex-row flex-wrap justify-center  lg:justify-around">
+            {/* charts */}
+            <div className="mx-5 my-4 h-32 w-32 border border-green-500"></div>
+            <div className="mx-5 my-4 h-32 w-32 border border-green-500"></div>
+            <div className="mx-5 my-4 h-32 w-32 border border-green-500"></div>
+          </div>
         </div>
 
-        <div className="md:col-span-6 md:p-10 lg:col-span-7 xl:col-span-8 ">
+        <div className="">
           <h2 className="mb-6 text-2xl font-semibold">
             <span className="text-green-500">Recipe</span> summary
           </h2>
