59c7c17ecd2d9973547d33c08cb40148e4676d88
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-08-05 13:30:09 +0200

    feat: add route guard

---
+++ app-wrappers/RouterGuard.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+
+export { RouteGuard };
+
+type Props = {
+  children: any;
+};
+
+function RouteGuard({ children }: Props) {
+  const router = useRouter();
+  const [authorized, setAuthorized] = useState(false);
+  const userId = useSelector((state: RootState) => state.user.uid);
+
+  useEffect(() => {
+    // on initial load - run auth check
+
+    authCheck(router.pathname);
+
+    // on route change start - hide page content by setting authorized to false
+    const hideContent = () => setAuthorized(false);
+
+    router.events.on("routeChangeStart", hideContent);
+
+    // on route change complete - run auth check
+    router.events.on("routeChangeComplete", authCheck);
+
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off("routeChangeStart", hideContent);
+      router.events.off("routeChangeComplete", authCheck);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId]);
+
+  function authCheck(url: string) {
+    // redirect to login page if accessing a private page and not logged in
+    const publicPaths = ["/login", "/register", "/account/[id]", "/", "/about"];
+    const path = url.split("?")[0];
+    console.log({ router });
+    console.log(userId);
+    if (userId === "" && !publicPaths.includes(path)) {
+      setAuthorized(false);
+      router.push("/login");
+    } else {
+      setAuthorized(true);
+    }
+  }
+
+  return authorized && children;
+}
+
+export default RouteGuard;
--- pages/account/[id]/index.tsx
+++ pages/account/[id]/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { NextPage, NextPageContext } from "next";
+import React, { useEffect, useState } from "react";
+import { NextPage } from "next";
 import Layout from "../../../layouts/Layout";
 import ProfileData from "../../../components/ProfileData";
 import FoodCard from "../../../components/FoodCard";
@@ -9,20 +9,25 @@ import { doc, onSnapshot } from "@firebase/firestore";
 import { db } from "../../../utils/firebase.utils";
 import { useRouter } from "next/router";
 
-const Account: NextPage<UserInformation> = (props) => {
+const Account: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [userData, setUserData] = useState<UserInformation>();
 
   useEffect(() => {
     const docRef = doc(db, "users", String(id));
 
-    const updateProps = async () => {
-      await router.replace(router.asPath);
-    };
     const unsub = onSnapshot(
       docRef,
-      async () => {
-        await updateProps();
+      async (snapshot) => {
+        setUserData({
+          name: snapshot.data()?.name,
+          email: snapshot.data()?.email,
+          age: snapshot.data()?.age,
+          recipes: snapshot.data()?.recipes,
+          height: snapshot.data()?.height,
+          weight: snapshot.data()?.weight,
+        });
       },
       (e) => {
         console.error(e.message);
@@ -37,19 +42,19 @@ const Account: NextPage<UserInformation> = (props) => {
     <Layout>
       <div className="mx-4 grid gap-12 pt-8 md:mx-12 md:grid-cols-12  xl:mx-32">
         <ProfileData
-          email={props.email}
-          name={props.name}
-          age={props.age}
-          height={props.height}
-          weight={props.weight}
+          email={userData?.email}
+          name={userData?.name}
+          age={userData?.age}
+          height={userData?.height}
+          weight={userData?.weight}
         />
         <div className=" md:col-span-6 lg:col-span-7 xl:col-span-8">
           <h2 className="mb-6 text-2xl font-semibold">
             <span className="text-green-500">Your</span> favorite recipes
           </h2>
           <div className="grid grid-cols-2 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {props.recipes &&
-              props.recipes.map((item: Recipe) => (
+            {userData?.recipes &&
+              userData.recipes.map((item: Recipe) => (
                 <FoodCard
                   id={item.id}
                   key={item.id}
@@ -69,15 +74,3 @@ const Account: NextPage<UserInformation> = (props) => {
 };
 
 export default Account;
-
-export const getServerSideProps = async (context: NextPageContext) => {
-  const { id } = context.query;
-  const response = await fetch(
-    `${process.env.APP_URL}/api/get_user_by_id?uid=${id}`
-  );
-  const data = await response.json();
-
-  return {
-    props: { ...data },
-  };
-};
