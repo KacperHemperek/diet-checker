deb7f4576eb123920ae74909d5b1c8a4fce43a6a
michal-pawlicki <michalpawlicki02@gmail.com>
2022-08-05 12:30:37 +0200

    fix: home page create acc button swapped to account button when logged in

--- components/AboutHero.tsx
+++ components/AboutHero.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
 import HeroImage from "../public/undraw_diet_ghvw.svg";
+import { RootState } from "../redux/store";
 import HeroComponent from "./HeroComponent";
 
 const AboutHero = () => {
+  const uid = useSelector((state: RootState) => state.user.uid);
   return (
     <>
       <HeroComponent img={HeroImage}>
@@ -16,18 +19,33 @@ const AboutHero = () => {
           don't have to count all the micro and macro elements yourself, we'll
           do it for you!
         </p>
-        <Link href="/register">
-          <button className="group mt-6 flex items-baseline gap-2 rounded-lg border border-green-500 fill-green-500 px-4 py-1 text-green-500 transition-all hover:bg-green-500 hover:fill-white hover:text-white">
-            Create Account{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              className="h-3 w-3 transition-transform duration-1000 group-hover:translate-x-1"
-            >
-              <path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z" />
-            </svg>
-          </button>
-        </Link>
+        {uid ? (
+          <Link href={`account/${uid}`}>
+            <button className="group mt-6 flex items-baseline gap-2 rounded-lg border border-green-500 fill-green-500 px-4 py-1 text-green-500 transition-all hover:bg-green-500 hover:fill-white hover:text-white">
+              Your Diets{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="h-3 w-3 transition-transform duration-1000 group-hover:translate-x-1"
+              >
+                <path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z" />
+              </svg>
+            </button>
+          </Link>
+        ) : (
+          <Link href="/register">
+            <button className="group mt-6 flex items-baseline gap-2 rounded-lg border border-green-500 fill-green-500 px-4 py-1 text-green-500 transition-all hover:bg-green-500 hover:fill-white hover:text-white">
+              Create Account{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="h-3 w-3 transition-transform duration-1000 group-hover:translate-x-1"
+              >
+                <path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z" />
+              </svg>
+            </button>
+          </Link>
+        )}
       </HeroComponent>
     </>
   );
